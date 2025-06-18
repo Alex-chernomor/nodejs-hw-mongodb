95c489d35dbff54ae088e15d6d2a5ed85c18feef
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-06-18 20:57:15 +0200

    Update upload.js

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -13,29 +13,3 @@ const storage = multer.diskStorage({
 });
 
 export const upload = multer({ storage });
-
-
-// import multer from 'multer';
-// import path from 'path';
-// import { nanoid } from 'nanoid';
-
-// const tempDir = path.resolve('tmp'); // Временная директория
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, tempDir);
-//   },
-//   filename: (req, file, cb) => {
-//     const ext = path.extname(file.originalname);
-//     cb(null, nanoid() + ext);
-//   },
-// });
-
-// const upload = multer({
-//   storage,
-//   limits: {
-//     fileSize: 5 * 1024 * 1024, // до 5MB
-//   },
-// });
-
-// export { upload };
